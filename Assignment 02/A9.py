c2e71cb409228fb0c79f93cee5476653e5ed2943
ovibaridar <ovibaridar06@gmail.com>
2023-11-02 18:45:01 +0600

    - , + , or 0

---
+++ Assignment 02/A9.py
@@ -0,0 +1,8 @@
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print(number, "is a positive number.")
+elif number < 0:
+    print(number, "is a negative number.")
+else:
+    print(number, "is zero.")
