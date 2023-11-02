4c75368d8ee8ac16070c3a552e3a036462da3293
ovibaridar <ovibaridar06@gmail.com>
2023-11-02 18:38:13 +0600

    average

---
+++ Assignment 02/A5.py
@@ -0,0 +1,10 @@
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+
+
+average = float(num1 + num2 + num3) / 3
+
+
+print("The average of the three numbers is:", average)
