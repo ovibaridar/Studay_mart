b5b59ac7a32082054ed51ce772235d2544badbcf
ovibaridar <ovibaridar06@gmail.com>
2023-11-02 18:40:58 +0600

    Max number

---
+++ Assignment 02/A7.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+max_number = max(num1, num2, num3)
+
+print("The maximum of the three numbers is:", max_number)
